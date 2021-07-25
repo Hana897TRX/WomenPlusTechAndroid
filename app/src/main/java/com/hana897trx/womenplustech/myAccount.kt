2f5476cb470ae3595890e792a6c4a25fbd7a01c4
hana897trx <nemesisdarkness897gt@gmail.com>
2021-07-25 11:18:23 -0500

    Profile UI ready

--- app/src/main/java/com/hana897trx/womenplustech/myAccount.kt
+++ app/src/main/java/com/hana897trx/womenplustech/myAccount.kt
@@ -1,22 +1,57 @@
 package com.hana897trx.womenplustech
 
+import android.app.DatePickerDialog
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.annotation.RequiresApi
+import androidx.cardview.widget.CardView
+import java.util.*
 
 class myAccount : Fragment() {
+    private lateinit var calendar : Calendar
+    private var year : Int = 1
+    private var month : Int = 1
+    private var day : Int = 1
+    private var date : DatePickerDialog? = null
+    private var txtBirthDay : TextView? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    @RequiresApi(Build.VERSION_CODES.N)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view =  inflater.inflate(R.layout.fragment_my_account, container, false)
+
+        addFunctions(view)
+        calendarConfig(view)
+
+        return view
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_account, container, false)
+    @RequiresApi(Build.VERSION_CODES.N)
+    private fun addFunctions(view : View) {
+        val cardImg = view.findViewById<CardView>(R.id.cardProfileImg)
+        val profileImg = view.findViewById<ImageView>(R.id.imgProfile)
+        val btnUpdate = view.findViewById<Button>(R.id.btnUpdate)
+        txtBirthDay = view.findViewById<TextView>(R.id.txtBirthdate)
+
+        txtBirthDay!!.setOnClickListener { date!!.show() }
+        btnUpdate.setOnClickListener { updateInformation() }
+
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    private fun calendarConfig (view : View) {
+        date = DatePickerDialog(requireContext(), DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+            txtBirthDay!!.text = "" + dayOfMonth + "/" + (month + 1)  + "/" + year
+        }, year, month, day)
+    }
+
+    private fun updateInformation() {
+
     }
 }
